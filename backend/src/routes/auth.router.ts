504504fb89c812eb62ccbf87263592a5c789486e
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-10 23:03:34 +0200

    chore: Update auth router to use tryCatch for error handling

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { login, register } from "../controllers/auth.controller";
 import { checkEmail } from "../middlewares/auth.middleware";
+import { tryCatch } from "../utils/tryCatch";
 
-const authRouter = Router();
+const authRouter: Router = Router();
 
-authRouter.post("/register", checkEmail, register);
-authRouter.post("/login", login);
+authRouter.post("/auth/register", tryCatch(checkEmail), tryCatch(register));
+authRouter.post("/auth/login", tryCatch(login));
 
 export default authRouter;
