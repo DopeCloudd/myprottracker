06eb101471966c378b0cfddcdc098cb4b4614255
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-10 23:03:25 +0200

    chore: Update getProductById and getProductByCategoryId to use schema for data validation

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,28 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Product } from "@prisma/client";
 import { Request, Response } from "express";
+import { categorySchema } from "../schemas/category.schema";
+import { productSchema } from "../schemas/product.schema";
 
 const productClient = new PrismaClient().product;
 
+// Schema for getting a product by id or by category id
+const productIdSchema = productSchema.pick({ id: true });
+const categoryIdSchema = categorySchema.pick({ id: true });
+
 // Get all products
 export const getProducts = async (req: Request, res: Response) => {
-  const products = await productClient.findMany();
+  const products: Product[] = await productClient.findMany();
   res.status(200).json(products);
 };
 
 // Get product by id
 export const getProductById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const product = await productClient.findUnique({
-    where: { id: parseInt(id) },
+  const { id } = productIdSchema.parse(req.params);
+  const product = await productClient.findUniqueOrThrow({
+    where: { id: id },
   });
   res.status(200).json(product);
 };
 
 // Get product by category id
 export const getProductByCategoryId = async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const { id } = categoryIdSchema.parse(req.params);
   const products = await productClient.findMany({
-    where: { categoryId: parseInt(id) },
+    where: { categoryId: id },
   });
   res.status(200).json(products);
 };
