80764051cef58f7b86d27f816450ab3dd3438cbd
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-10 23:03:30 +0200

    chore: Refactor getUserById to use user schema for data validation

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { Request, Response } from "express";
+import { userSchema } from "../schemas/user.schema";
 
 const userClient = new PrismaClient().user;
 
+const userIdSchema = userSchema.pick({ id: true });
+
 // Get user by id
 export const getUserById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const user = await userClient.findUnique({
-    where: { id: String(id) },
+  const { id } = userIdSchema.parse(req.params);
+  const user: User | null = await userClient.findUniqueOrThrow({
+    where: { id: id },
   });
   res.status(200).json(user);
 };
