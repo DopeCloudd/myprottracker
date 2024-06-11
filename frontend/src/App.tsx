610b4a458d52bf3044a3e24d02c43b055b2db9c0
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-11 19:42:58 +0200

    chore: Update App.tsx to include routing and theming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <></>;
-}
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./interface/pages/Landing";
+import Login from "./interface/pages/Login";
+import { darkTheme } from "./interface/ui/theme";
 
-export default App;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+}
