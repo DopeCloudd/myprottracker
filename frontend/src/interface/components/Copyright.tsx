70860fe693045acbe1564e5ac10762a0994105bf
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-11 19:41:28 +0200

    feat: Add Copyright component

---
+++ frontend/src/interface/components/Copyright.tsx
@@ -0,0 +1,16 @@
+import { Link, Typography } from "@mui/material";
+import { SxProps, Theme } from "@mui/system";
+
+interface CopyrightProps {
+  sx?: SxProps<Theme>;
+}
+
+export const Copyright: React.FC<CopyrightProps> = ({ sx }) => {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" sx={sx}>
+      {"Copyright © "}
+      <Link href="https://mui.com/">Myprottracker&nbsp;</Link>
+      {new Date().getFullYear()}
+    </Typography>
+  );
+};
