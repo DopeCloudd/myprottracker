69cebee4138fa5020d038b817f11b33c68942d31
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-11 14:50:09 +0200

    chore: Update backend entry point to use app module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,7 @@
-import bodyParser from "body-parser";
-import compression from "compression";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
-import { errorHandler } from "./middlewares/error.middleware";
-import authRouter from "./routes/auth.router";
-import productRouter from "./routes/product.router";
-import userRouter from "./routes/user.router";
+import app from "./app";
 
 dotenv.config();
-
-const app = express();
-
-app.use(
-  cors({
-    /* origin: "http://localhost:3000",
-    credentials: true, */
-  }),
-);
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
-// Routes
-app.use(authRouter);
-app.use(userRouter);
-app.use(productRouter);
-// Error handler
-app.use(errorHandler);
 
 const PORT = process.env.API_PORT || 3032;
 app.listen(PORT, () => {
