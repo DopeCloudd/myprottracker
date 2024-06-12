a5b15e879680d6b889421ed5360c135b7835257f
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-12 12:40:49 +0200

    chore: Add Logo component for displaying the application logo

---
+++ frontend/src/interface/components/navbar/Logo.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+
+type LogoProps = {
+  imageUrl: string;
+  handleClick: () => void;
+};
+
+export default function Logo(props: LogoProps) {
+  return (
+    <Box
+      sx={{
+        height: { xs: "30px", sm: "40px" },
+        width: { xs: "30px", sm: "40px" },
+      }}
+    >
+      <img
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+        src={props.imageUrl}
+        alt="MyProtTracker Logo"
+        onClick={props.handleClick}
+      />
+    </Box>
+  );
+}
