52ef0edd1185e996652395af33eb5c77fa8d9c6a
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-12 12:42:48 +0200

    feat: Add react-router-dom for navigation and implement login and logout functionality

--- frontend/src/interface/components/navbar/AuthButton.tsx
+++ frontend/src/interface/components/navbar/AuthButton.tsx
@@ -2,6 +2,7 @@ import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { Button, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import { User } from "../../../domain/entities/user.type";
 
 type AuthButtonProps = {
@@ -11,14 +12,22 @@ type AuthButtonProps = {
 
 export default function AuthButton(props: AuthButtonProps) {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    console.log("Logout");
+  };
 
   return (
-    <Button variant="outlined">
-      {props.user ? (
-        <LogoutIcon fontSize="small" sx={{ mr: { xs: 0, md: 1 } }} />
-      ) : (
-        <LoginIcon fontSize="small" sx={{ mr: { xs: 0, md: 1 } }} />
-      )}
+    <Button
+      variant="outlined"
+      startIcon={props.user ? <LogoutIcon /> : <LoginIcon />}
+      onClick={props.user ? handleLogout : handleLogin}
+    >
       <Typography
         component="span"
         sx={{
