c893933a044a359eee5b288f3d3b8e5f57f3cef1
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-12 12:42:56 +0200

    chore: Update App component with new theme and language provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,34 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { I18nextProvider } from "react-i18next";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import i18n from "./infrastructure/translate/i18n";
+import Navbar from "./interface/components/Navbar";
 import Landing from "./interface/pages/Landing";
 import Login from "./interface/pages/Login";
-import { darkTheme } from "./interface/ui/theme";
+import { themeSettings } from "./interface/ui/theme";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  const darkTheme = useMemo(() => createTheme(themeSettings), []);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </ThemeProvider>
+        <I18nextProvider i18n={i18n}>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Box>
+              <Navbar />
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Landing />} />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </I18nextProvider>
       </QueryClientProvider>
     </BrowserRouter>
   );
