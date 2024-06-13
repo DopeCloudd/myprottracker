cb0599ca2dd059226b4fe86d9d671bb6e012c5ad
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-14 00:55:43 +0200

    feat: Update Navbar component with user avatar and authentication functionality

--- frontend/src/interface/components/Navbar.tsx
+++ frontend/src/interface/components/Navbar.tsx
@@ -1,16 +1,18 @@
+import { useAppSelector } from "@/infrastructure/store/hooks";
+import TitleOblique from "@/interface/components/TitleOblique";
+import FlexBetween from "@/interface/components/mui/FlexBetween";
+import FlexCenter from "@/interface/components/mui/FlexCenter";
+import AuthButton from "@/interface/components/navbar/AuthButton";
+import AvatarDropdown from "@/interface/components/navbar/AvatarDropdown";
+import Logo from "@/interface/components/navbar/Logo";
+import SwitchLangage from "@/interface/components/navbar/SwitchLangage";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import PersonIcon from "@mui/icons-material/Person";
 import { useNavigate } from "react-router-dom";
-import TitleOblique from "./TitleOblique";
-import FlexBetween from "./global/FlexBetween";
-import FlexCenter from "./global/FlexCenter";
-import AuthButton from "./navbar/AuthButton";
-import AvatarDropdown from "./navbar/AvatarDropdown";
-import Logo from "./navbar/Logo";
-import SwitchLangage from "./navbar/SwitchLangage";
 
 function Navbar() {
+  const user = useAppSelector((state) => state.auth.basicUserInfo);
   const navigate = useNavigate();
 
   const LinkItem = [
@@ -50,21 +52,20 @@ function Navbar() {
     <FlexBetween
       sx={{
         height: "100px",
-        px: 6,
       }}
     >
-      <FlexCenter>
+      <FlexCenter gap={2}>
         <Logo imageUrl="/icon.png" handleClick={handleButtonClick} />
         <TitleOblique
           text="MY<span>PROT</span>TRACKER"
-          sx={{ ml: 2, fontSize: "clamp(1rem, 0.4375rem + 1.5vw, 1.75rem)" }}
+          sx={{ fontSize: "clamp(1rem, 0.4375rem + 1.5vw, 1.75rem)" }}
           onClick={handleButtonClick}
         />
       </FlexCenter>
       <FlexCenter>
         <SwitchLangage />
-        <AvatarDropdown textAvatar="Valentin" items={LinkItem} />
-        <AuthButton user={null} onClick={handleLogin} />
+        <AvatarDropdown textAvatar={user?.firstName} items={LinkItem} />
+        <AuthButton user={user} onClick={handleLogin} />
       </FlexCenter>
     </FlexBetween>
   );
