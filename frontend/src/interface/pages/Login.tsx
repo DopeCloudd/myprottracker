cccd8af9444d0611183427589949ebd4263c9390
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-14 00:56:22 +0200

    refactor: Update Login page components and structure

--- frontend/src/interface/pages/Login.tsx
+++ frontend/src/interface/pages/Login.tsx
@@ -1,26 +1,8 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import {
-  Avatar,
-  Box,
-  Button,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { Copyright } from "../components/Copyright";
-import TitleOblique from "../components/TitleOblique";
+import TitleOblique from "@/interface/components/TitleOblique";
+import { Form } from "@/interface/components/global/Form";
+import { Box, Grid, Typography } from "@mui/material";
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
-
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
       <Grid
@@ -41,7 +23,10 @@ export default function Login() {
             height: "100%",
           }}
         >
-          <TitleOblique fontSize="3rem" />
+          <TitleOblique
+            text="MY<span>PROT</span>TRACKER"
+            sx={{ fontSize: "3rem" }}
+          />
           <Typography
             component="h2"
             variant="body1"
@@ -51,87 +36,7 @@ export default function Login() {
           </Typography>
         </Box>
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={5}
-        sx={{
-          backgroundColor: "#171717",
-        }}
-      >
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar
-            sx={{
-              m: 1,
-              bgcolor: "transparent",
-              border: "1px solid",
-              borderColor: "#00A656",
-            }}
-          >
-            <LockOutlinedIcon sx={{ fill: "#00A656" }} />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ mt: 5 }} />
-          </Box>
-        </Box>
-      </Grid>
+      <Form type="login" />
     </Grid>
   );
 }
