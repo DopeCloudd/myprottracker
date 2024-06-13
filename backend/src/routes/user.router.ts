31b905f30e8002f38e33afe636d0e97fee794ce0
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-14 00:56:47 +0200

    refactor: Add JWT token verification middleware

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { getUserById } from "../controllers/user.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 import { tryCatch } from "../utils/tryCatch";
 
 const userRouter = Router();
 
-userRouter.get("/user/:id", tryCatch(getUserById));
+userRouter.get("/user/:id", verifyToken, tryCatch(getUserById));
 
 export default userRouter;
