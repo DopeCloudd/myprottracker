bc62f6d11bb4dedc8e86c8bf2d0e597ef939a8d4
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-14 21:39:52 +0200

    refactor: Remove unused code and dependencies in auth.middleware.ts

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
 
 const userClient = new PrismaClient().user;
 
@@ -13,37 +12,10 @@ export const checkEmail = async (
   const { email } = req.body;
   const user = await userClient.findUnique({
     where: { email },
+    select: { email: true },
   });
   if (user) {
     throw new Error("Email is already in use");
   }
-  next();
-};
-
-export const verifyToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-
-  let token = req.header("Authorization");
-
-  if (!token) {
-    throw new Error("Access denied");
-  }
-
-  if (token.startsWith("Bearer ")) {
-    token = token.slice(7, token.length).trimStart();
-  }
-
-  const verified = jwt.verify(token, process.env.JWT_SECRET);
-
-  if (!verified) {
-    throw new Error("Invalid token");
-  }
-
   next();
 };
