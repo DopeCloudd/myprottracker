d9ca8e518a335194e8778f452d768fb664c8c030
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-15 23:39:47 +0200

    refactor: Update store.ts to use combined reducers

--- frontend/src/infrastructure/store/store.ts
+++ frontend/src/infrastructure/store/store.ts
@@ -1,14 +1,16 @@
 import authReducer from "@/domain/usecases/auth.slice";
 import productsReducer from "@/domain/usecases/product.slice";
 import userReducer from "@/domain/usecases/user.slice";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  user: userReducer,
+  products: productsReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    user: userReducer,
-    products: productsReducer,
-  },
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
