c6e7cf71ba00023f3e18dc139859deb581122256
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-15 23:40:23 +0200

    refactor: Add lazy loading for images in Landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
+import useToken from "@/infrastructure/hooks/useToken";
+import i18n from "@/infrastructure/translate/i18n";
+import Layout from "@/interface/layout";
+import Landing from "@/interface/pages/Landing";
+import Login from "@/interface/pages/Login";
+import Register from "@/interface/pages/Register";
+import { themeSettings } from "@/interface/ui/theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { I18nextProvider } from "react-i18next";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import i18n from "./infrastructure/translate/i18n";
-import Layout from "./interface/layout";
-import Landing from "./interface/pages/Landing";
-import Login from "./interface/pages/Login";
-import Register from "./interface/pages/Register";
-import { themeSettings } from "./interface/ui/theme";
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  useToken();
+
   const darkTheme = useMemo(() => createTheme(themeSettings), []);
   return (
     <BrowserRouter>
