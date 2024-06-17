ed1dc4380026c4e187950da12cdcfb60e4b3f0ce
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 00:48:29 +0200

    refactor: Remove console.log statement in refreshToken function

--- frontend/src/application/redux/slices/auth.slice.ts
+++ frontend/src/application/redux/slices/auth.slice.ts
@@ -141,7 +141,6 @@ export const refreshToken = createAsyncThunk<
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log(response.data);
     return response.data;
   } catch (error) {
     if (error instanceof Error) {
