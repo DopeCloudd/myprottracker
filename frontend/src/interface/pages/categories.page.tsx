f77c5f5cbd81c306fa02c8e0f729a45405be7fef
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 00:48:12 +0200

    refactor: Update Categories page to fetch and display category data

--- frontend/src/interface/pages/categories.page.tsx
+++ frontend/src/interface/pages/categories.page.tsx
@@ -1,3 +1,24 @@
+import { useGetCategoriesQuery } from "@/infrastructure/api/category.api";
+
 export default function Categories() {
-  return <div>Categories</div>;
+  const { data, isFetching, isLoading } = useGetCategoriesQuery();
+
+  if (isFetching) {
+    return <div>Fetching...</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {data?.map((category) => (
+        <div key={category.id}>
+          <h1>{category.name}</h1>
+          <p>{category.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
