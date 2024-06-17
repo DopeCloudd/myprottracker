583ae5b70c21e76b9a054c58e1d12ed03b648a5a
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-17 23:08:37 +0200

    refactor: Add FlexColumnCenter component for centered flex column layout

---
+++ frontend/src/interface/components/mui/FlexColumnCenter.tsx
@@ -0,0 +1,10 @@
+import { Box, styled } from "@mui/material";
+
+const FlexColumnCenter = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export default FlexColumnCenter;
