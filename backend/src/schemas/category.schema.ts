0a8c34781ede32ff7d20b64425145f953d07093c
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 00:47:11 +0200

    feat: Add description field to Category schema

--- backend/src/schemas/category.schema.ts
+++ backend/src/schemas/category.schema.ts
@@ -3,4 +3,7 @@ import { z } from "zod";
 export const categorySchema = z.object({
   id: z.number(),
   name: z.string(),
+  description: z.string(),
 });
+
+export const categoryIdSchema = categorySchema.pick({ id: true });
