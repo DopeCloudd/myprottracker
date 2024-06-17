eb349f3b7056a35954800151f42d65275efb2ed2
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 00:48:50 +0200

    refactor: Update store configuration to include category API and middleware

--- frontend/src/application/redux/store.ts
+++ frontend/src/application/redux/store.ts
@@ -1,17 +1,22 @@
 import authReducer from "@/application/redux/slices/auth.slice";
 import categoriesReducer from "@/application/redux/slices/category.slice";
 import productsReducer from "@/application/redux/slices/product.slice";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { categoryApi } from "@/infrastructure/api/category.api";
+import { combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit/react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   products: productsReducer,
   categories: categoriesReducer,
+  [categoryApi.reducerPath]: categoryApi.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(categoryApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
