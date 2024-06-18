452239987a87722bfca93a9d3ce6f8e7890b620a
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 22:06:56 +0200

    refactor: Create PasswordInput component for handling password input

---
+++ frontend/src/interface/components/input/password-input.component.tsx
@@ -0,0 +1,52 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import React, { useState } from "react";
+
+export default function PasswordInput({
+  name,
+  onChange,
+}: {
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <FormControl fullWidth variant="outlined">
+      <InputLabel htmlFor="outlined-adornment-password">{name}</InputLabel>
+      <OutlinedInput
+        fullWidth
+        id="outlined-adornment-password"
+        type={showPassword ? "text" : "password"}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+        label="Password"
+        onChange={onChange}
+      />
+    </FormControl>
+  );
+}
