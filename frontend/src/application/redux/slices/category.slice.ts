3058b10fe2ac64b44e4a7d886bea890b8e638386
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 22:06:03 +0200

    refactor: Update category slice to use categoryId instead of categories array

--- frontend/src/application/redux/slices/category.slice.ts
+++ frontend/src/application/redux/slices/category.slice.ts
@@ -1,65 +1,21 @@
-import { Category } from "@/domain/entities/category.types";
-import { Status } from "@/domain/entities/status.type";
-import fetchClient from "@/infrastructure/api/fetch.instance";
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-type CategoriesState = {
-  categories: Category[];
-  loading: Status;
-  error: string | null;
+type CategoryState = {
+  categoryId: number | null;
 };
 
-const initialState: CategoriesState = {
-  categories: [],
-  loading: Status.IDLE,
-  error: null,
+const initialState: CategoryState = {
+  categoryId: null,
 };
-
-export const fetchCategories = createAsyncThunk<
-  Category[],
-  void,
-  { rejectValue: string }
->("categories/fetchCategories", async (_, { rejectWithValue }) => {
-  try {
-    const response = await fetchClient<Category[]>({
-      method: "GET",
-      endpoint: "/categories",
-    });
-    return response.data;
-  } catch (error) {
-    if (error instanceof Error) {
-      return rejectWithValue(error.message);
-    } else {
-      return rejectWithValue("An unknown error occurred");
-    }
-  }
-});
-
-const productsSlice = createSlice({
-  name: "categories",
+const categorySlice = createSlice({
+  name: "category",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchCategories.pending, (state) => {
-        state.loading = Status.LOADING;
-        state.error = null;
-      })
-      .addCase(
-        fetchCategories.fulfilled,
-        (state, action: PayloadAction<Category[]>) => {
-          state.categories = action.payload;
-          state.loading = Status.FULFILLED;
-        }
-      )
-      .addCase(
-        fetchCategories.rejected,
-        (state, action: PayloadAction<string | undefined>) => {
-          state.loading = Status.REJECTED;
-          state.error = action.payload ?? "Failed to fetch categories";
-        }
-      );
+  reducers: {
+    setCategoryId: (state, action: PayloadAction<number>) => {
+      state.categoryId = action.payload;
+    },
   },
 });
 
-export default productsSlice.reducer;
+export const { setCategoryId } = categorySlice.actions;
+export default categorySlice.reducer;
