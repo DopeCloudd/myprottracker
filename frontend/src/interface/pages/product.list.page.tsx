a100c386891254ca62d0c0650e5573d20d0ae6d1
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 22:06:35 +0200

    refactor: Update ProductList component to fetch and display products by category ID

--- frontend/src/interface/pages/product.list.page.tsx
+++ frontend/src/interface/pages/product.list.page.tsx
@@ -1,3 +1,44 @@
-export default function ProductList() {
-  return <div>ProductList</div>;
-}
+import { useGetProductsByCategoryIdQuery } from "@/infrastructure/api/product.api";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const ProductList: React.FC = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!id) {
+      navigate("/categories");
+    }
+  }, [id, navigate]);
+
+  if (!id) {
+    return null;
+  }
+
+  return <ProductListWithQuery categoryId={Number(id)} />;
+};
+
+const ProductListWithQuery: React.FC<{ categoryId: number }> = ({
+  categoryId,
+}) => {
+  const { data, isLoading } = useGetProductsByCategoryIdQuery(categoryId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || data.length === 0) {
+    return <div>No products found.</div>;
+  }
+
+  return (
+    <div>
+      {data.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
