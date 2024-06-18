31d75e590234c16ec00c711e5d5b68e5bbcc68ce
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 22:06:45 +0200

    refactor: Import FlexColumnCenter component in landing page

--- frontend/src/interface/pages/landing.page.tsx
+++ frontend/src/interface/pages/landing.page.tsx
@@ -1,9 +1,9 @@
+import FlexColumnCenter from "@/interface/components/box/flex-column-center.component";
 import CallToAction from "@/interface/components/landing/CallToAction";
 import Hero from "@/interface/components/landing/Hero";
 import Highlights from "@/interface/components/landing/Highlights";
 import Pricing from "@/interface/components/landing/Pricing";
 import Title from "@/interface/components/landing/Title";
-import FlexColumnCenter from "@/interface/components/mui/FlexColumnCenter";
 
 export default function Landing() {
   return (
