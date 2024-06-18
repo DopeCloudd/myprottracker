7ad3f07321e6e75036c571379411628c4d0e9184
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-18 22:06:13 +0200

    refactor: Update store configuration to include product API and middleware

--- frontend/src/application/redux/store.ts
+++ frontend/src/application/redux/store.ts
@@ -1,7 +1,8 @@
 import authReducer from "@/application/redux/slices/auth.slice";
-import categoriesReducer from "@/application/redux/slices/category.slice";
+import categoryReducer from "@/application/redux/slices/category.slice";
 import productsReducer from "@/application/redux/slices/product.slice";
 import { categoryApi } from "@/infrastructure/api/category.api";
+import { productApi } from "@/infrastructure/api/product.api";
 import { combineReducers } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit/react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
@@ -9,14 +10,18 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 const rootReducer = combineReducers({
   auth: authReducer,
   products: productsReducer,
-  categories: categoriesReducer,
+  category: categoryReducer,
   [categoryApi.reducerPath]: categoryApi.reducer,
+  [productApi.reducerPath]: productApi.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(categoryApi.middleware),
+    getDefaultMiddleware().concat(
+      categoryApi.middleware,
+      productApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
