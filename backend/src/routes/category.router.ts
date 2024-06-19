87094d3c02b6416d2603a6e2d990bd252433cadf
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-19 23:56:56 +0200

    refactor: Add getCategoryById and getCategoryByName endpoints to category router

--- backend/src/routes/category.router.ts
+++ backend/src/routes/category.router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getCategories } from "../controllers/category.controller";
+import {
+  getCategories,
+  getCategoryById,
+  getCategoryByName,
+} from "../controllers/category.controller";
 import { tryCatch } from "../utils/tryCatch";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/categories", tryCatch(getCategories));
+categoryRouter.get("/category/id/:id", tryCatch(getCategoryById));
+categoryRouter.get("/category/name/:name", tryCatch(getCategoryByName));
 
 export default categoryRouter;
