63a0b9201101211e2c53cdd70d8703244bed3889
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-19 22:06:32 +0200

    feat: Create CardSkeleton component for displaying a skeleton card with title, description, and image

---
+++ frontend/src/interface/components/card/card-skeleton.component.tsx
@@ -0,0 +1,40 @@
+import {
+  CardActionArea,
+  CardContent,
+  Card as MuiCard,
+  Skeleton,
+} from "@mui/material";
+import React from "react";
+
+type CardSkeletonProps = {
+  image: boolean;
+};
+
+const CardSkeleton: React.FC<CardSkeletonProps> = ({ image }) => {
+  return (
+    <MuiCard
+      sx={{
+        backgroundColor: "#171717",
+        boxShadow: "0px 0px 15px 2px #0c0c0c",
+        borderRadius: "10px",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+        }}
+      >
+        {image && <Skeleton variant="rectangular" width={140} height={140} />}
+        <CardContent sx={{ width: "100%" }}>
+          <Skeleton variant="text" width={200} height={40} />
+          <Skeleton variant="text" width={"100%"} height={100} />
+        </CardContent>
+      </CardActionArea>
+    </MuiCard>
+  );
+};
+
+export default CardSkeleton;
