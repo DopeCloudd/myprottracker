fb6c99dea8b3d418b3b706b3da169e0e94be43c4
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-20 00:32:41 +0200

    refactor: Update Card component to include price, quantity, and rating

--- frontend/src/interface/components/card/card.component.tsx
+++ frontend/src/interface/components/card/card.component.tsx
@@ -1,20 +1,36 @@
+import Flex from "@/interface/components/box/flex.component";
+import TextPrice from "@/interface/components/text/text-price.component";
 import {
   CardActionArea,
   CardContent,
   CardMedia,
   Card as MuiCard,
+  Rating,
   Typography,
 } from "@mui/material";
 import React from "react";
+import FlexEnd from "../box/flex-end.component";
+import ButtonBuy from "../button/button-buy.component";
 
 type CardProps = {
   title: string;
   description: string;
+  price?: number;
+  quantity?: string;
+  rating?: number;
   image?: string;
   onClick?: () => void;
 };
 
-const Card: React.FC<CardProps> = ({ title, description, image, onClick }) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  price,
+  quantity,
+  rating,
+  image,
+  onClick,
+}) => {
   return (
     <MuiCard
       sx={{
@@ -49,9 +65,37 @@ const Card: React.FC<CardProps> = ({ title, description, image, onClick }) => {
           >
             {title}
           </Typography>
+          <Flex>
+            {price && <TextPrice price={price} type="card" />}
+            {price && quantity && <div style={{ width: "1rem" }}></div>}
+            {quantity && (
+              <Typography
+                component="p"
+                sx={{
+                  fontSize: "1rem",
+                  opacity: "0.5",
+                }}
+              >
+                {quantity}
+              </Typography>
+            )}
+          </Flex>
+          {rating && (
+            <Rating
+              name="rating"
+              defaultValue={rating}
+              precision={0.1}
+              readOnly
+            />
+          )}
           <Typography variant="body1" color="text.secondary">
             {description}
           </Typography>
+          {price && (
+            <FlexEnd>
+              <ButtonBuy content="Buy" type="card" onClick={onClick} />
+            </FlexEnd>
+          )}
         </CardContent>
       </CardActionArea>
     </MuiCard>
