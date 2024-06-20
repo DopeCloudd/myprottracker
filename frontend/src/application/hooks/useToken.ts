4c822bfad70bbd756cdfc311f189c49f129cf4e4
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-21 00:02:36 +0200

    refactor: Update useToken hook to refresh token and set credentials

--- frontend/src/application/hooks/useToken.ts
+++ frontend/src/application/hooks/useToken.ts
@@ -1,24 +1,31 @@
-import { logout, refreshToken } from "@/application/redux/slices/auth.slice";
+import { logout, setCredentials } from "@/application/redux/slices/auth.slice";
 import { useAppDispatch } from "@/application/redux/store";
+import { useRefreshTokenMutation } from "@/infrastructure/api/auth.api";
 import { jwtDecode } from "jwt-decode";
 import { useEffect } from "react";
 
 const useToken = () => {
+  const [refreshToken] = useRefreshTokenMutation();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const decodedToken: { exp: number } = jwtDecode(token);
-      const currentTime = Date.now() / 1000;
+    const verifyToken = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const decodedToken: { exp: number } = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
 
-      if (decodedToken.exp > currentTime) {
-        dispatch(refreshToken(token));
-      } else {
-        dispatch(logout());
+        if (decodedToken.exp > currentTime) {
+          const user = await refreshToken(token).unwrap();
+          dispatch(setCredentials(user));
+        } else {
+          dispatch(logout());
+        }
       }
-    }
-  }, [dispatch]);
+    };
+
+    verifyToken();
+  }, [dispatch, refreshToken]);
 };
 
 export default useToken;
