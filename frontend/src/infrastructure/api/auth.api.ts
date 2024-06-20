bb5e066e78b3a9f74d93735af6bdc814b8f67d92
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-21 00:02:24 +0200

    refactor: Add auth.api.ts for handling authentication API requests

---
+++ frontend/src/infrastructure/api/auth.api.ts
@@ -0,0 +1,55 @@
+import { RootState } from "@/application/redux/store";
+import {
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+} from "@/domain/entities/user.type";
+import { BASEURL } from "@/infrastructure/api/index";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASEURL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation<LoginResponse, LoginRequest>({
+      query: (credentials) => ({
+        url: "auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    register: builder.mutation<{ message: string }, RegisterRequest>({
+      query: (credentials) => ({
+        url: "auth/register",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    refreshToken: builder.mutation<LoginResponse, string>({
+      query: (token) => ({
+        url: "auth/refresh-token",
+        method: "POST",
+        body: { token },
+      }),
+    }),
+    protected: builder.mutation<{ message: string }, void>({
+      query: () => "protected",
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useRefreshTokenMutation,
+  useProtectedMutation,
+} = authApi;
