0a74161e0e21ba40285e7c82341a3dc0249705fb
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-22 21:45:53 +0200

    refactor: Update App component to use SnackbarProvider for displaying snackbars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import Register from "@/interface/pages/register.page";
 import { themeSettings } from "@/interface/theme/theme";
 import { Box, ThemeProvider, createTheme } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+import { SnackbarProvider } from "notistack";
 import { useMemo } from "react";
 import { I18nextProvider } from "react-i18next";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -21,29 +22,31 @@ export default function App() {
   const darkTheme = useMemo(() => createTheme(themeSettings), []);
   return (
     <BrowserRouter>
-      <I18nextProvider i18n={i18n}>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Box>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route element={<Layout />}>
-                <Route path="/" element={<Landing />} />
-                <Route path="categories" element={<Categories />} />
-                <Route
-                  path="categories/:id/products/"
-                  element={<ProductList />}
-                />
-                <Route path="product/:id" element={<Product />} />
-                <Route element={<PrivateOutlet />}>
-                  <Route path="profile" element={<div>Porfile</div>} />
+      <SnackbarProvider maxSnack={1}>
+        <I18nextProvider i18n={i18n}>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Box>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Landing />} />
+                  <Route path="categories" element={<Categories />} />
+                  <Route
+                    path="categories/:id/products/"
+                    element={<ProductList />}
+                  />
+                  <Route path="product/:id" element={<Product />} />
+                  <Route element={<PrivateOutlet />}>
+                    <Route path="profile" element={<div>Porfile</div>} />
+                  </Route>
                 </Route>
-              </Route>
-            </Routes>
-          </Box>
-        </ThemeProvider>
-      </I18nextProvider>
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </I18nextProvider>
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
