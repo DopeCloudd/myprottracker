495d31350b6625b2f0d9926be24adce132b6053f
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-22 21:45:10 +0200

    refactor: Update store configuration to include favorites and alerts slices, and their respective API modules

--- frontend/src/application/redux/store.ts
+++ frontend/src/application/redux/store.ts
@@ -1,8 +1,11 @@
+import alertsReducer from "@/application/redux/slices/alerts.slice";
 import authReducer from "@/application/redux/slices/auth.slice";
 import categoryReducer from "@/application/redux/slices/category.slice";
-import productsReducer from "@/application/redux/slices/product.slice";
+import favoritesReducer from "@/application/redux/slices/favorites.slice";
+import { alertApi } from "@/infrastructure/api/alert.api";
 import { authApi } from "@/infrastructure/api/auth.api";
 import { categoryApi } from "@/infrastructure/api/category.api";
+import { favoriteApi } from "@/infrastructure/api/favorite.api";
 import { productApi } from "@/infrastructure/api/product.api";
 import { combineReducers } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit/react";
@@ -10,11 +13,14 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  products: productsReducer,
+  favorites: favoritesReducer,
+  alerts: alertsReducer,
   category: categoryReducer,
   [authApi.reducerPath]: authApi.reducer,
   [categoryApi.reducerPath]: categoryApi.reducer,
   [productApi.reducerPath]: productApi.reducer,
+  [favoriteApi.reducerPath]: favoriteApi.reducer,
+  [alertApi.reducerPath]: alertApi.reducer,
 });
 
 export const store = configureStore({
@@ -23,7 +29,9 @@ export const store = configureStore({
     getDefaultMiddleware().concat(
       authApi.middleware,
       categoryApi.middleware,
-      productApi.middleware
+      productApi.middleware,
+      favoriteApi.middleware,
+      alertApi.middleware
     ),
 });
 
