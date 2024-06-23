12e2213936cd16fde60bf786b6fe9e35299e4031
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-23 15:52:31 +0200

    refactor: Include product details in alert controller responses

--- backend/src/controllers/alert.controller.ts
+++ backend/src/controllers/alert.controller.ts
@@ -11,17 +11,32 @@ export const addAlert = async (req: Request, res: Response) => {
       userId: userId,
       productId: productId,
     },
+    include: { product: true },
   });
-  res.status(201).json(alert);
+
+  if (!alert) {
+    throw new Error("Alert not created");
+  }
+
+  res.status(201).json(alert.product);
 };
 
 // Remove an alert for a user
 export const removeAlert = async (req: Request, res: Response) => {
   const { userId, productId } = req.body;
-  const alert = await alertClient.deleteMany({
+  await alertClient.deleteMany({
     where: { userId: userId, productId: productId },
   });
-  res.status(200).json(alert);
+  const alerts = await alertClient.findMany({
+    where: { userId: userId },
+    include: {
+      product: true,
+    },
+  });
+
+  const products = alerts.map((alert) => alert.product);
+
+  res.status(200).json(products);
 };
 
 // Get all alerts for a user
@@ -29,6 +44,8 @@ export const getAlerts = async (req: Request, res: Response) => {
   const { userId } = req.params;
   const alerts = await alertClient.findMany({
     where: { userId: userId },
+    include: { product: true },
   });
-  res.status(200).json(alerts);
+  const products = alerts.map((alert) => alert.product);
+  res.status(200).json(products);
 };
