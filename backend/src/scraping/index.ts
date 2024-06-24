f6c71a49f8b02ae063c12c22ab335b6bb62078d8
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-24 15:57:39 +0200

    refactor: Add scraping functionality for multiple brands

---
+++ backend/src/scraping/index.ts
@@ -0,0 +1,34 @@
+import { getUrls } from "./helpers/get-urls.helper";
+import { initBrowser } from "./puppeteer/index";
+import { Bulk } from "./scrapers/bulk.scraper";
+import { Eafit } from "./scrapers/eafit.scraper";
+import { Myprotein } from "./scrapers/myprotein.scraper";
+import { Prozis } from "./scrapers/prozis.scraper";
+import { Config } from "./types";
+
+const config: Config = {
+  title: true,
+  price: true,
+  quantity: true,
+  description: true,
+  imageUrl: true,
+  brand: true,
+  url: true,
+  category: true,
+};
+
+(async () => {
+  const bulk_urls = await getUrls("Bulk");
+  const eafit_urls = await getUrls("Eafit");
+  const myprotein_urls = await getUrls("Myprotein");
+  const prozis_urls = await getUrls("Prozis");
+  const browser = await initBrowser();
+  const results = await Promise.all([
+    Bulk(browser, config, bulk_urls),
+    Eafit(browser, config, eafit_urls),
+    Myprotein(browser, config, myprotein_urls),
+    Prozis(browser, config, prozis_urls),
+  ]);
+  await browser.close();
+  console.log(results);
+})();
