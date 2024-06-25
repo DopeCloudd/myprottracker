85fbf52165d744cabe09d2a66640d1919b40eac4
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-25 18:21:49 +0200

    feat: Add loading state to AdminOutlet component

--- frontend/src/interface/components/route/admin-outlet.component.tsx
+++ frontend/src/interface/components/route/admin-outlet.component.tsx
@@ -1,9 +1,19 @@
 import { useAuth } from "@/application/hooks/useAuth";
+import FlexCenter from "@/interface/components/box/flex-center.component";
+import { CircularProgress } from "@mui/material";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export function AdminOutlet() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
+
+  if (loading) {
+    return (
+      <FlexCenter flex={1}>
+        <CircularProgress />
+      </FlexCenter>
+    );
+  }
 
   return user?.role === "ADMIN" ? (
     <Outlet />
