ff8e5546ab40b9a58fcfe090a340aa149e1128e7
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-25 18:21:45 +0200

    refactor: Add loading state to PrivateOutlet component

--- frontend/src/interface/components/route/private-outlet.component.tsx
+++ frontend/src/interface/components/route/private-outlet.component.tsx
@@ -1,11 +1,21 @@
 import { useAuth } from "@/application/hooks/useAuth";
+import FlexCenter from "@/interface/components/box/flex-center.component";
+import { CircularProgress } from "@mui/material";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export function PrivateOutlet() {
-  const auth = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
 
-  return auth.user ? (
+  if (loading) {
+    return (
+      <FlexCenter flex={1}>
+        <CircularProgress />
+      </FlexCenter>
+    );
+  }
+
+  return user ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} />
