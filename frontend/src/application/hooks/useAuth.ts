4f45d990c8a5ff433c4a491a4eda12ccce8d498f
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-25 18:21:25 +0200

    refactor: Update useAuth hook to use auth.slice selectors

--- frontend/src/application/hooks/useAuth.ts
+++ frontend/src/application/hooks/useAuth.ts
@@ -1,9 +1,13 @@
-import { RootState } from "@/application/redux/store";
+import {
+  loadingCurrentUser,
+  selectCurrentUser,
+} from "@/application/redux/slices/auth.slice";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 export const useAuth = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const user = useSelector(selectCurrentUser);
+  const loading = useSelector(loadingCurrentUser);
 
-  return useMemo(() => ({ user }), [user]);
+  return useMemo(() => ({ user, loading }), [user, loading]);
 };
