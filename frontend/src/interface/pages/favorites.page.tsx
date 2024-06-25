b8193405ca77f3c8612ce6c927edd8abc7f51120
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-25 19:05:10 +0200

    refactor: Add lazy loading for images in Favorites page and update import paths

--- frontend/src/interface/pages/favorites.page.tsx
+++ frontend/src/interface/pages/favorites.page.tsx
@@ -3,22 +3,34 @@ import useFetchUserFavorites from "@/application/hooks/useFetchUserFavorites";
 import { selectFavorites } from "@/application/redux/slices/favorites.slice";
 import { useTypedSelector } from "@/application/redux/store";
 import { bufferToImageSrc } from "@/infrastructure/helpers/buffer-to-image-src.helper";
+import FlexCenter from "@/interface/components/box/flex-center.component";
 import CardSkeleton from "@/interface/components/card/card-skeleton.component";
 import Card from "@/interface/components/card/card.component";
 import TextTitle from "@/interface/components/text/text-title.component";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Favorites: React.FC = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const { favoriteLoading } = useFetchUserFavorites(user?.id || null);
 
   const favorites = useTypedSelector((state) => selectFavorites(state)) || [];
 
   const navigate = useNavigate();
 
-  if (!user) return null;
+  if (loading) {
+    return (
+      <FlexCenter flex={1}>
+        <CircularProgress />
+      </FlexCenter>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
   return (
     <Box
@@ -42,7 +54,27 @@ const Favorites: React.FC = () => {
             <CardSkeleton key={index} image={true} />
           ))
         ) : !favorites || favorites.length === 0 ? (
-          <div>No products found.</div>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gridColumn: "span 2",
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Vous n'avez pas encore de favoris pour l'instant.
+            </Typography>
+            <Button
+              variant="outlined"
+              onClick={() => {
+                navigate("/categories");
+              }}
+            >
+              Comparer des produits
+            </Button>
+          </Box>
         ) : (
           favorites.map((product) => (
             <Card
