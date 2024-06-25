7075cb39b3b17ed3f727e337315d4643445e4bd8
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-25 18:32:41 +0200

    refactor: Rename Navbar component to navbar.component and update import paths

--- frontend/src/interface/components/nav/navbar.component.tsx
+++ frontend/src/interface/components/nav/navbar.component.tsx
@@ -1,11 +1,11 @@
 import { useAuth } from "@/application/hooks/useAuth";
-import TitleOblique from "@/interface/components/TitleOblique";
 import FlexBetween from "@/interface/components/box/flex-between.component";
 import FlexCenter from "@/interface/components/box/flex-center.component";
 import { AuthButton } from "@/interface/components/navbar/AuthButton";
 import AvatarDropdown from "@/interface/components/navbar/AvatarDropdown";
 import Logo from "@/interface/components/navbar/Logo";
 import SwitchLangage from "@/interface/components/navbar/SwitchLangage";
+import TextTitleOblique from "@/interface/components/text/text-title-oblique.component";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import PersonIcon from "@mui/icons-material/Person";
@@ -46,6 +46,7 @@ function Navbar() {
 
   return (
     <FlexBetween
+      component="nav"
       sx={{
         height: "100px",
         px: 6,
@@ -57,7 +58,7 @@ function Navbar() {
         onClick={handleButtonClick}
       >
         <Logo imageUrl="/icon.png" />
-        <TitleOblique
+        <TextTitleOblique
           text="MY<span>PROT</span>TRACKER"
           sx={{ fontSize: "clamp(1rem, 0.4375rem + 1.5vw, 1.75rem)" }}
         />
