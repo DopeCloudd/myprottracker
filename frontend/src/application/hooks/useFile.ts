9e51477022a4758866e4560d8bf3ba2a9ea92ea3
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-27 20:56:19 +0200

    feat: Add useFile hook for handling file selection and preview

---
+++ frontend/src/application/hooks/useFile.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const useFile = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setSelectedFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleRemoveFile = () => {
+    setSelectedFile(null);
+    setPreview(null);
+  };
+
+  return {
+    selectedFile,
+    preview,
+    handleFileChange,
+    handleRemoveFile,
+  };
+};
+
+export default useFile;
