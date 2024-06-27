3418d86d11c5f3de648648135659aa70ebd7769d
Valentin Lerouge <cloudd2vi@gmail.com>
2024-06-27 20:55:36 +0200

    refactor: Add image upload functionality to createProduct route

--- backend/src/routes/product.router.ts
+++ backend/src/routes/product.router.ts
@@ -5,6 +5,7 @@ import {
   getProductById,
   getProducts,
 } from "../controllers/product.controller";
+import uploadMiddleware from "../middlewares/upload.middleware";
 import { tryCatch } from "../utils/tryCatch";
 
 const productRouter = Router();
@@ -12,5 +13,6 @@ const productRouter = Router();
 productRouter.get("/products", tryCatch(getProducts));
 productRouter.get("/products/:id", tryCatch(getProductById));
 productRouter.get("/products/category/:id", tryCatch(getProductByCategoryId));
+productRouter.post("/products", uploadMiddleware, tryCatch(getProducts));
 
 export default productRouter;
