078c7d3fe696977d5723a76ceca635dc8db3b555
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-04 19:36:25 +0200

    refactor: Update admin page to use API for retrieving categories and brands

--- frontend/src/interface/pages/admin.page.tsx
+++ frontend/src/interface/pages/admin.page.tsx
@@ -1,4 +1,6 @@
 import useFile from "@/application/hooks/useFile";
+import { useGetBrandsQuery } from "@/infrastructure/api/brand.api";
+import { useGetCategoriesQuery } from "@/infrastructure/api/category.api";
 import { useAddProductMutation } from "@/infrastructure/api/product.api";
 import Loading from "@/interface/components/global/Loading";
 import InputFileUpload from "@/interface/components/input/file-upload-input.component";
@@ -17,47 +19,26 @@ import { useSnackbar } from "notistack";
 import React from "react";
 import * as yup from "yup";
 
-const categories = [
-  {
-    value: 1,
-    label: "Whey",
-  },
-  {
-    value: 2,
-    label: "Gainer",
-  },
-];
-
-const brand = [
-  {
-    value: "Bulk",
-    label: "Bulk",
-  },
-  {
-    value: "Eafit",
-    label: "Eafit",
-  },
-];
-
 const schema = yup.object().shape({
   url: yup.string().required("Merci de renseigner l'URL du produit"),
   category: yup
     .string()
     .required("Merci de renseigner la catégorie du produit"),
   brand: yup.string().required("Merci de renseigner la marque du produit"),
-  image: yup.mixed().required("Merci de sélectionner une image"),
 });
 
 const initialValues = {
   url: "",
   category: "",
   brand: "",
-  image: null,
 };
 
 const Admin: React.FC = () => {
   const { enqueueSnackbar } = useSnackbar();
   const { file, handleRemoveFile } = useFile();
+  const { data: categories, isLoading: categoriesLoad } =
+    useGetCategoriesQuery();
+  const { data: brands, isLoading: brandsLoad } = useGetBrandsQuery();
   const [addProduct, { isLoading }] = useAddProductMutation();
 
   const formik = useFormik({
@@ -75,7 +56,7 @@ const Admin: React.FC = () => {
         const formData = new FormData();
         const { url, category, brand } = values;
         formData.append("url", url);
-        formData.append("brand", brand);
+        formData.append("brandId", brand);
         formData.append("categoryId", parseInt(category).toString());
         formData.append("image", file);
         try {
@@ -99,39 +80,6 @@ const Admin: React.FC = () => {
       }
     },
   });
-
-  /* const onSubmit = async (values: {
-    url: string;
-    category: string;
-    brand: string;
-  }) => {
-    if (file) {
-      const formData = new FormData();
-      const { url, category, brand } = values;
-      formData.append("url", url);
-      formData.append("brand", brand);
-      formData.append("categoryId", parseInt(category).toString());
-      formData.append("image", file);
-      try {
-        const product = await addProduct(formData).unwrap();
-        if (product) {
-          resetForm();
-          handleRemoveFile();
-          enqueueSnackbar("Produit ajouté avec succès", {
-            variant: "success",
-          });
-        }
-      } catch (error) {
-        enqueueSnackbar("Erreur lors de l'ajout du produit", {
-          variant: "error",
-        });
-      }
-    } else {
-      enqueueSnackbar("Veuillez sélectionner une image", {
-        variant: "error",
-      });
-    }
-  }; */
 
   return (
     <Box
@@ -168,48 +116,54 @@ const Admin: React.FC = () => {
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  select
-                  label="Catégorie du produit"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  name="category"
-                  value={formik.values.category}
-                  error={
-                    Boolean(formik.touched.category) &&
-                    Boolean(formik.errors.category)
-                  }
-                  helperText={formik.touched.category && formik.errors.category}
-                >
-                  {categories.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
+                <Loading loading={categoriesLoad}>
+                  <TextField
+                    fullWidth
+                    select
+                    label="Catégorie du produit"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    name="category"
+                    value={formik.values.category}
+                    error={
+                      Boolean(formik.touched.category) &&
+                      Boolean(formik.errors.category)
+                    }
+                    helperText={
+                      formik.touched.category && formik.errors.category
+                    }
+                  >
+                    {categories?.map((option) => (
+                      <MenuItem key={option.id} value={option.id}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Loading>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  select
-                  label="Marque du produit"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  name="brand"
-                  value={formik.values.brand}
-                  error={
-                    Boolean(formik.touched.brand) &&
-                    Boolean(formik.errors.brand)
-                  }
-                  helperText={formik.touched.brand && formik.errors.brand}
-                >
-                  {brand.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
+                <Loading loading={brandsLoad}>
+                  <TextField
+                    fullWidth
+                    select
+                    label="Marque du produit"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    name="brand"
+                    value={formik.values.brand}
+                    error={
+                      Boolean(formik.touched.brand) &&
+                      Boolean(formik.errors.brand)
+                    }
+                    helperText={formik.touched.brand && formik.errors.brand}
+                  >
+                    {brands?.map((option) => (
+                      <MenuItem key={option.id} value={option.id}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Loading>
               </Grid>
               <Grid item xs={12}>
                 <InputFileUpload />
