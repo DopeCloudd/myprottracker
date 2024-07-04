4387d14515e3ba41dc45e14ad85cd449cee11255
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-04 22:22:16 +0200

    refactor: Update getUrls helper to use brand name in where clause

--- backend/src/scraping/helpers/get-urls.helper.ts
+++ backend/src/scraping/helpers/get-urls.helper.ts
@@ -5,7 +5,7 @@ const productClient = new PrismaClient().product;
 export function getUrls(brand: string): Promise<string[]> {
   return productClient
     .findMany({
-      where: { brand },
+      where: { brand: { name: brand } },
       select: { url: true },
     })
     .then((urls) => urls.map((product) => product.url));
