78a7cd78b27f9417e419c2317f87643862e1f5a5
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-09 13:48:39 +0200

    chore: Add profile page to frontend

---
+++ frontend/src/interface/pages/profile.page.tsx
@@ -0,0 +1,87 @@
+import { useAuth } from "@/application/hooks/useAuth";
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import TextTitle from "../components/text/text-title.component";
+
+const Profile: React.FC = () => {
+  const { user } = useAuth();
+  return (
+    <Box sx={{ px: 6 }}>
+      <TextTitle content="Profil" />
+      <Typography variant="h3" sx={{ mb: 2 }}>
+        Informations personnelles
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="email"
+            label="Email"
+            value={user?.email}
+            disabled
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="firstName"
+            label="Prénom"
+            value={user?.firstName}
+            disabled
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="lastName"
+            label="Nom"
+            value={user?.lastName}
+            disabled
+          />
+        </Grid>
+      </Grid>
+      <Divider sx={{ my: 4 }} />
+      <Typography variant="h3" sx={{ mb: 2 }}>
+        Abonnement
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Abonnement</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={user?.subscription || "FREE"}
+              label="Abonnement"
+              readOnly
+            >
+              <MenuItem value={"FREE"}>FREE</MenuItem>
+              <MenuItem value={"PREMIUM"}>PREMIUM</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sx={{ display: "flex", gap: 2 }}>
+          <Button variant="outlined" color="warning" fullWidth>
+            Modifier
+          </Button>
+          <Button variant="outlined" color="error" fullWidth>
+            Annuler
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Profile;
