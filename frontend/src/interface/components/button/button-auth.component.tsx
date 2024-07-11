e8236e978f28b2793be782c23b6c843bdc565f34
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-11 13:24:46 +0200

    refactor: Update ButtonAuth component for responsive design

--- frontend/src/interface/components/button/button-auth.component.tsx
+++ frontend/src/interface/components/button/button-auth.component.tsx
@@ -3,11 +3,14 @@ import { logout } from "@/application/redux/slices/auth.slice";
 import { useAppDispatch } from "@/application/redux/store";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, useMediaQuery, useTheme } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
 export const ButtonAuth: React.FC = () => {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.down("md"));
   const { user } = useAuth();
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
@@ -22,19 +25,22 @@ export const ButtonAuth: React.FC = () => {
   };
 
   return (
-    <Button
-      variant="outlined"
-      startIcon={user ? <LogoutIcon /> : <LoginIcon />}
-      onClick={user ? handleLogout : handleLogin}
-    >
-      <Typography
-        component="span"
-        sx={{
-          display: { xs: "none", md: "block" },
-        }}
-      >
-        {user ? t("nav.logout") : t("nav.login")}
-      </Typography>
-    </Button>
+    <>
+      {isXs ? (
+        <IconButton color="primary" onClick={user ? handleLogout : handleLogin}>
+          {user ? <LogoutIcon /> : <LoginIcon />}
+        </IconButton>
+      ) : (
+        <Button
+          variant="outlined"
+          startIcon={user ? <LogoutIcon /> : <LoginIcon />}
+          onClick={user ? handleLogout : handleLogin}
+        >
+          <Typography component="span">
+            {user ? t("nav.logout") : t("nav.login")}
+          </Typography>
+        </Button>
+      )}
+    </>
   );
 };
