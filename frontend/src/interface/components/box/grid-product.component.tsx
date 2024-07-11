8a1bbc800786059390539e75bc3085e1b7cb7eb2
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-11 13:24:39 +0200

    refactor: Update GridProduct component for responsive design

--- frontend/src/interface/components/box/grid-product.component.tsx
+++ frontend/src/interface/components/box/grid-product.component.tsx
@@ -1,11 +1,18 @@
 import { Box, styled } from "@mui/material";
 
-const GridProduct = styled(Box)({
+const GridProduct = styled(Box)(({ theme }) => ({
+  position: "relative",
   display: "grid",
   gridTemplateColumns: "50% 1fr",
   gridTemplateRows: "1fr",
   gridColumnGap: "0px",
   gridRowGap: "0px",
-}) as typeof Box;
+  [theme.breakpoints.down("md")]: {
+    gridTemplateColumns: "1fr", // Pour les écrans moyens et plus petits
+  },
+  [theme.breakpoints.down("sm")]: {
+    gridTemplateColumns: "1fr", // Pour les petits écrans
+  },
+})) as typeof Box;
 
 export default GridProduct;
