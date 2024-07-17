cc4e9cacbf95392ac790474d5d5aad6b49c2fa60
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-17 14:06:33 +0200

    refactor: Add Nutrition type for product nutrition values

---
+++ frontend/src/domain/entities/nutrition.type.ts
@@ -0,0 +1,11 @@
+export type Nutrition = {
+  productId: number;
+  calories: number | null;
+  protein: number | null;
+  fat: number | null;
+  saturatedFat: number | null;
+  carbohydrates: number | null;
+  sugar: number | null;
+  fiber: number | null;
+  salt: number | null;
+};
