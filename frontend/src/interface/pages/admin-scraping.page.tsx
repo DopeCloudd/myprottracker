626309086f1f34253f763ff5bdb139090247640d
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-19 14:14:55 +0200

    feat: Add functionality to start scraping all products

--- frontend/src/interface/pages/admin-scraping.page.tsx
+++ frontend/src/interface/pages/admin-scraping.page.tsx
@@ -1,3 +1,4 @@
+import { useStartScrapingMutation } from "@/infrastructure/api/scraping.api";
 import LanguageIcon from "@mui/icons-material/Language";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -6,6 +7,23 @@ import Button from "@mui/material/Button";
 import React from "react";
 
 const AdminScraping: React.FC = () => {
+  const [startScraping] = useStartScrapingMutation();
+  const handleScraping = (action: string) => {
+    switch (action) {
+      case "new":
+        console.log("Scraping des nouveaux produits");
+        break;
+      case "all":
+        startScraping();
+        break;
+      case "some":
+        console.log("Scraping de certains produits");
+        break;
+      default:
+        console.log("Action non reconnue");
+        break;
+    }
+  };
   return (
     <Box
       sx={{
@@ -17,7 +35,11 @@ const AdminScraping: React.FC = () => {
       <Button variant="outlined" startIcon={<ShoppingCartIcon />}>
         Scraping des nouveaux produits
       </Button>
-      <Button variant="outlined" startIcon={<LanguageIcon />}>
+      <Button
+        variant="outlined"
+        startIcon={<LanguageIcon />}
+        onClick={() => handleScraping("all")}
+      >
         Scraping de tout les produits
       </Button>
       <Button variant="outlined" startIcon={<SettingsIcon />}>
