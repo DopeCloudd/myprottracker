b1de6923b98cded3faf793cd9b8228fa64c81bca
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-19 14:14:14 +0200

    refactor: Update product update endpoint to include nutrition values

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -90,6 +90,88 @@ export const createProduct = async (req: Request, res: Response) => {
   res.status(201).json(newProduct);
 };
 
+// Update a product
+export const updateProduct = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const values = req.body;
+  const url = String(values.url);
+  const categoryId = parseInt(values.categoryId);
+  const brandId = parseInt(values.brandId);
+  const image = req.file;
+  const nutrition_values = values.nutrition_values;
+
+  if (!url || !categoryId || !brandId) {
+    throw new Error("Missing required fields.");
+  }
+
+  const product = await productClient.findUnique({
+    where: { id: id },
+  });
+
+  if (!product) {
+    throw new Error("Product not found.");
+  }
+
+  const updatedProduct = await productClient.update({
+    where: { id: product.id },
+    data: {
+      url: url,
+      categoryId: categoryId,
+      brandId: brandId,
+      image: image ? image.buffer : product.image,
+    },
+  });
+
+  if (!updatedProduct) {
+    throw new Error("Failed to update product.");
+  }
+
+  const productNutrition = await nutritionClient.findFirst({
+    where: { productId: product.id },
+  });
+
+  if (!productNutrition) {
+    const createdProductNutrition = await nutritionClient.create({
+      data: {
+        productId: product.id,
+        calories: nutrition_values.calories ?? null,
+        protein: nutrition_values.protein ?? null,
+        fat: nutrition_values.fat ?? null,
+        saturatedFat: nutrition_values.saturatedFat ?? null,
+        carbohydrates: nutrition_values.carbohydrates ?? null,
+        sugar: nutrition_values.sugar ?? null,
+        fiber: nutrition_values.fiber ?? null,
+        salt: nutrition_values.salt ?? null,
+      },
+    });
+    if (!createdProductNutrition) {
+      throw new Error("Failed to create product nutrition values.");
+    }
+  } else {
+    const updatedProductNutrition = await nutritionClient.update({
+      where: { id: productNutrition.id },
+      data: {
+        calories: nutrition_values.calories ?? productNutrition.calories,
+        protein: nutrition_values.protein ?? productNutrition.protein,
+        fat: nutrition_values.fat ?? productNutrition.fat,
+        saturatedFat:
+          nutrition_values.saturatedFat ?? productNutrition.saturatedFat,
+        carbohydrates:
+          nutrition_values.carbohydrates ?? productNutrition.carbohydrates,
+        sugar: nutrition_values.sugar ?? productNutrition.sugar,
+        fiber: nutrition_values.fiber ?? productNutrition.fiber,
+        salt: nutrition_values.salt ?? productNutrition.salt,
+      },
+    });
+
+    if (!updatedProductNutrition) {
+      throw new Error("Failed to update product nutrition values.");
+    }
+  }
+
+  res.status(200).json(updatedProduct);
+};
+
 // Delete a product
 export const deleteProduct = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
@@ -103,6 +185,7 @@ export const deleteProduct = async (req: Request, res: Response) => {
 
   await productClient.delete({
     where: { id: id },
+    include: { nutrition: true },
   });
 
   res.status(204).send();
