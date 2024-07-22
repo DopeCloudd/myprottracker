5a51f823b32685e962b1cafdf53e04991c498858
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-22 13:54:20 +0200

    refactor: Correct typo in saturedFat field name in Nutrition type

--- frontend/src/domain/entities/nutrition.type.ts
+++ frontend/src/domain/entities/nutrition.type.ts
@@ -3,7 +3,7 @@ export type Nutrition = {
   calories: number | null;
   protein: number | null;
   fat: number | null;
-  saturatedFat: number | null;
+  saturedFat: number | null;
   carbohydrates: number | null;
   sugar: number | null;
   fiber: number | null;
