f43962f711b2f7427c712895e7829a54c7cc1d62
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-26 13:38:56 +0200

    refactor: Update SectionAccordion component to use styled components

--- frontend/src/interface/components/accordion/section.accordion.tsx
+++ frontend/src/interface/components/accordion/section.accordion.tsx
@@ -1,21 +1,35 @@
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  styled,
+} from "@mui/material";
 import React, { ReactNode } from "react";
+
+const StyledAccordion = styled(Accordion)(({ theme }) => ({
+  background: "transparent",
+  boxShadow: "none",
+  borderTop: "1px solid rgba(255,255,255, 0.5)",
+  borderBottom: "1px solid rgba(255,255,255, 0.5)",
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  "&:first-of-type": {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  "&:last-of-type": {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+}));
 
 const SectionAccordion: React.FC<{
   header: string;
   content: string | ReactNode;
 }> = ({ header, content }) => {
   return (
-    <Accordion
-      sx={{
-        background: "transparent",
-        boxShadow: "none",
-        borderTop: "1px solid rgba(255,255,255, 0.5)",
-        borderBottom: "1px solid rgba(255,255,255, 0.5)",
-        my: 2,
-      }}
-    >
+    <StyledAccordion>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel2-content"
@@ -34,7 +48,7 @@ const SectionAccordion: React.FC<{
       >
         {content}
       </AccordionDetails>
-    </Accordion>
+    </StyledAccordion>
   );
 };
 
