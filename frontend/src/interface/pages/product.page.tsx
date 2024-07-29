c6a752bf35fe4cae876074080060200006f77202
Valentin Lerouge <cloudd2vi@gmail.com>
2024-07-29 13:54:39 +0200

    refactor: Update NutritionTable component styling and add lazy loading for images

--- frontend/src/interface/pages/product.page.tsx
+++ frontend/src/interface/pages/product.page.tsx
@@ -1,15 +1,17 @@
-import { useGetProductByIdQuery } from "@/infrastructure/api/product.api";
+import useProductPage from "@/application/hooks/useProductPage";
 import { bufferToImageSrc } from "@/infrastructure/helpers/buffer-to-image-src.helper";
 import { removeAccents } from "@/infrastructure/helpers/remove-accents.helper";
-import GridProduct from "@/interface/components/box/grid-product.component";
+import SectionAccordion from "@/interface/components/accordion/section.accordion";
 import { ButtonAlert } from "@/interface/components/button/button-alert.component";
 import ButtonBuy from "@/interface/components/button/button-buy.component";
 import { ButtonLike } from "@/interface/components/button/button-like.component";
-import { SkeletonProduct } from "@/interface/components/skeleton/skeleton-product.component";
+import Card from "@/interface/components/card/card.component";
+import NutritionTable from "@/interface/components/table/nutrition.table";
 import TextPrice from "@/interface/components/text/text-price.component";
 import TextProductSection from "@/interface/components/text/text-product-section.component";
+import Loading from "@/interface/layout/loading.layout";
 import { truncateString } from "@/interface/utils/index";
-import { Box, Button, Divider, Rating, Typography } from "@mui/material";
+import { Box, Grid, Rating, Typography, styled } from "@mui/material";
 import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 
@@ -23,8 +25,22 @@ const Product: React.FC = () => {
   return <ProductQuery productId={Number(id)} />;
 };
 
+const Row = styled(Box)({
+  width: "100%",
+  display: "flex",
+}) as typeof Box;
+
 const ProductQuery: React.FC<{ productId: number }> = ({ productId }) => {
-  const { data: product, isLoading } = useGetProductByIdQuery(productId);
+  const { product, productLoading, randomProducts, randomProductsLoading } =
+    useProductPage({ id: productId });
+
+  if (productLoading || randomProductsLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
 
   return (
     <Box
@@ -32,145 +48,108 @@ const ProductQuery: React.FC<{ productId: number }> = ({ productId }) => {
         pt: 4,
         px: { xs: 3, sm: 6 },
         pb: 4,
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <GridProduct>
-        {isLoading ? (
-          <SkeletonProduct />
-        ) : !product ? (
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          gap: 2,
+          justifyContent: "flex-end",
+        }}
+      >
+        <ButtonAlert productId={product.id} />
+        <ButtonLike productId={product.id} />
+      </Box>
+      <Loading loading={[productLoading, randomProductsLoading]}>
+        <Row
+          sx={{
+            position: "relative",
+            gap: 4,
+          }}
+        >
           <Box
             sx={{
-              gridColumn: "span 2",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-              mt: 4,
+              width: "50%",
+              "& img": {
+                aspectRatio: "4/4",
+                width: "700px",
+              },
+            }}
+          >
+            <img
+              src={bufferToImageSrc(product.image.data)}
+              alt={product.title}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: "50%",
             }}
           >
             <Typography
               variant="h1"
               component="h1"
               sx={{
+                fontFamily: "Integral Oblique, sans-serif",
                 fontSize: "2rem",
+                mt: { xs: 0, sm: 2 },
               }}
             >
-              Oops, il semblerait que ce produit n'existe pas.
+              {removeAccents(product.title)}
             </Typography>
-            <Button
-              variant="outlined"
-              color="primary"
-              sx={{
-                mt: 2,
-              }}
-              onClick={() => {
-                window.history.back();
-              }}
-            >
-              Retour
-            </Button>
-          </Box>
-        ) : (
-          <>
+            <Rating
+              name="read-only"
+              value={product.rating}
+              precision={0.1}
+              readOnly
+              sx={{ mt: 2 }}
+            />
             <Box
               sx={{
-                px: 4,
-                "& img": {
-                  aspectRatio: "4/4",
-                  width: "100%",
-                },
+                display: "flex",
+                alignItems: "baseline",
               }}
             >
-              <img
-                src={bufferToImageSrc(product.image.data)}
-                alt={product.title}
-              />
+              <TextPrice price={product.price} type="product" />
+              <Typography
+                component="p"
+                sx={{
+                  opacity: "0.5",
+                  fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
+                }}
+              >
+                {product.quantity}
+              </Typography>
             </Box>
+            <Typography
+              component="p"
+              sx={{
+                opacity: "0.5",
+                fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
+              }}
+            >
+              Vendu par {product.brand.name}
+            </Typography>
+            <Typography
+              component="p"
+              sx={{
+                opacity: "0.5",
+                fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
+              }}
+            >
+              Prix le plus bas : {product.lowestPrice} € · Prix le plus haut :{" "}
+              {product.highestPrice} €
+            </Typography>
+            <ButtonBuy
+              content="Acheter"
+              onClick={() => {
+                window.open(product.url, "_blank");
+              }}
+            />
             <Box>
-              <Box
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  right: 0,
-                  px: { xs: 3, sm: 6 },
-                  width: "100%",
-                  display: "flex",
-                  gap: 2,
-                  justifyContent: "flex-end",
-                }}
-              >
-                <ButtonAlert productId={product.id} />
-                <ButtonLike productId={product.id} />
-              </Box>
-              <Typography
-                variant="h1"
-                component="h1"
-                sx={{
-                  fontFamily: "Integral Oblique, sans-serif",
-                  fontSize: "2rem",
-                  mt: { xs: 0, sm: 2 },
-                }}
-              >
-                {removeAccents(product.title)}
-              </Typography>
-              <Rating
-                name="read-only"
-                value={product.rating}
-                precision={0.1}
-                readOnly
-                sx={{ mt: 2 }}
-              />
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "baseline",
-                }}
-              >
-                <TextPrice price={product.price} type="product" />
-                <Typography
-                  component="p"
-                  sx={{
-                    opacity: "0.5",
-                    fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
-                  }}
-                >
-                  {product.quantity}
-                </Typography>
-              </Box>
-              <Typography
-                component="p"
-                sx={{
-                  opacity: "0.5",
-                  fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
-                }}
-              >
-                Vendu par {product.brand.name}
-              </Typography>
-              <Typography
-                component="p"
-                sx={{
-                  opacity: "0.5",
-                  fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
-                }}
-              >
-                Prix le plus bas : {product.lowestPrice} €
-              </Typography>
-              <Typography
-                component="p"
-                sx={{
-                  opacity: "0.5",
-                  fontSize: "clamp(0.875rem, 0.6071rem + 0.7143vw, 1.25rem)",
-                }}
-              >
-                Prix le plus haut : {product.highestPrice} €
-              </Typography>
-              <ButtonBuy
-                content="Acheter"
-                onClick={() => {
-                  window.open(product.url, "_blank");
-                }}
-              />
               <TextProductSection content={"Analyse"} />
               <Typography
                 component="p"
@@ -182,25 +161,66 @@ const ProductQuery: React.FC<{ productId: number }> = ({ productId }) => {
                 Notre analyse de la composition de ce produit a pu mettre en
                 avant que ...
               </Typography>
-              <TextProductSection content={"Description"} />
-              <Typography
-                component="p"
-                sx={{ fontSize: "clamp(1rem, 0.7188rem + 0.75vw, 1.375rem)" }}
-              >
-                {truncateString(product.description, 400)}
-              </Typography>
             </Box>
-          </>
-        )}
-      </GridProduct>
-      <Divider
-        variant="middle"
-        sx={{
-          mt: 4,
-          mb: 2,
-        }}
-      />
-      <Typography variant="h3">Vous pourriez aussi aimé</Typography>
+          </Box>
+        </Row>
+        <Row
+          sx={{
+            gap: 4,
+          }}
+        >
+          <Box
+            sx={{
+              width: "50%",
+            }}
+          >
+            <SectionAccordion
+              header="Description"
+              content={truncateString(product.description, 400)}
+            />
+            <SectionAccordion
+              header="Valeurs nutrionnelles"
+              content={<NutritionTable />}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: "50%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "Integral Oblique, sans-serif",
+                fontSize: "1.5rem",
+                pb: 2,
+              }}
+            >
+              Vous pourriez aussi aimer
+            </Typography>
+            <Grid container spacing={2}>
+              {Array.from(Array(6).keys()).map((index) => (
+                <Grid item xs={6} key={index}>
+                  <Card
+                    key={index}
+                    title={"Product " + index}
+                    price={Math.floor(Math.random() * 100)}
+                    quantity={"100g"}
+                    rating={Math.floor(Math.random() * 5)}
+                    image={bufferToImageSrc(product.image.data)}
+                    description={"Description " + index}
+                    brand={"Brand " + index}
+                    onClick={() => {
+                      alert("clicked");
+                    }}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Row>
+      </Loading>
     </Box>
   );
 };
