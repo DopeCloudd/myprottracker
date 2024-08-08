27102c28edfa8dda9110b3a9f93a905f8ea29e0d
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-08 13:42:20 +0200

    refactor: Add RequestsTable component for displaying requests in a table

---
+++ frontend/src/interface/components/table/requests.table.tsx
@@ -0,0 +1,100 @@
+import useRequests from "@/application/hooks/useRequests";
+import Loading from "@/interface/layout/loading.layout";
+import { Box, Grid, Link, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { ReactNode } from "react";
+
+interface Column {
+  id: "id" | "url" | "email";
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  link?: (value: string) => ReactNode;
+}
+
+const columns: readonly Column[] = [
+  { id: "id", label: "ID" },
+  {
+    id: "url",
+    label: "URL",
+    link: (url) => {
+      return (
+        <Link href={url} target="_blank" color="inherit">
+          {url}
+        </Link>
+      );
+    },
+  },
+  { id: "email", label: "Email" },
+];
+
+export default function RequestsTable() {
+  const { rows, requestsLoading } = useRequests();
+
+  return (
+    <Box>
+      <Typography component="h1" variant="h2" mb={2}>
+        Liste des demandes
+      </Typography>
+      <Box>
+        <Loading loading={[requestsLoading]}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Paper sx={{ width: "100%", overflow: "hidden", mb: 6 }}>
+                <TableContainer sx={{ maxHeight: 600 }}>
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map((column) => (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                          >
+                            {column.label}
+                          </TableCell>
+                        ))}
+                        <TableCell align="center" colSpan={2}>
+                          Actions
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows?.map((row) => {
+                        return (
+                          <TableRow
+                            hover
+                            role="checkbox"
+                            tabIndex={-1}
+                            key={row.url}
+                          >
+                            {columns.map((column) => {
+                              const value = row[column.id];
+                              return (
+                                <TableCell key={column.id} align={column.align}>
+                                  {column.link && typeof value === "string"
+                                    ? column.link(value)
+                                    : value}
+                                </TableCell>
+                              );
+                            })}
+                          </TableRow>
+                        );
+                      })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Loading>
+      </Box>
+    </Box>
+  );
+}
