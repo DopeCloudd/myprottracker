e6a595dbd17811d0d921335a0c5882bea6d27149
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-13 13:49:44 +0200

    refactor: Update useStats hook to use new statistics API integration

--- frontend/src/application/hooks/useStats.ts
+++ frontend/src/application/hooks/useStats.ts
@@ -1,58 +1,10 @@
-import { useGetProductsQuery } from "@/infrastructure/api/product.api";
-import { useEffect, useState } from "react";
-
-interface Data {
-  id: number;
-  title: string;
-  url: string;
-  brand: string;
-  category: string;
-}
-
-function createData(
-  id: number,
-  title: string,
-  url: string,
-  brand: string,
-  category: string
-): Data {
-  return { id, url, title, brand, category };
-}
+import { useGetStatisticsQuery } from "@/infrastructure/api/statistics.api";
 
 const useStats = () => {
-  const { data: products, isLoading: productsLoading } = useGetProductsQuery();
-
-  const [rows, setRows] = useState(
-    () =>
-      products?.map((product) =>
-        createData(
-          product.id,
-          product.title,
-          product.url,
-          product.brand.name,
-          product.category.name
-        )
-      ) ?? []
-  );
-
-  useEffect(() => {
-    if (products) {
-      const updatedRows = products.map((product) =>
-        createData(
-          product.id,
-          product.title,
-          product.url,
-          product.brand.name,
-          product.category.name
-        )
-      );
-      setRows(updatedRows);
-    }
-  }, [products]);
+  const { data: stats } = useGetStatisticsQuery();
 
   return {
-    rows,
-    productsLoading,
+    stats,
   };
 };
 
