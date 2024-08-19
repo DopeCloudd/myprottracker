6ee891aea41a8fd020e30c64195e9a3130b54ec7
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-19 13:35:22 +0200

    refactor: Add bodyParser middleware for JSON parsing in stripe webhook route

--- backend/src/routes/stripe.router.ts
+++ backend/src/routes/stripe.router.ts
@@ -1,9 +1,14 @@
+import bodyParser from "body-parser";
 import { Router } from "express";
 import { webhook } from "../controllers/stripe.controller";
 import { tryCatch } from "../utils/tryCatch";
 
 const stripeRouter = Router();
 
-stripeRouter.post("/stripe/webhook", tryCatch(webhook));
+stripeRouter.post(
+  "/stripe/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  tryCatch(webhook),
+);
 
 export default stripeRouter;
