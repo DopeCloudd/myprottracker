5b0864b17bd425f6bb3cc9a862b8dc9504dc0dfb
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-19 13:35:25 +0200

    refactor: Add webhook event handling for checkout.session.completed event type

--- backend/src/controllers/stripe.controller.ts
+++ backend/src/controllers/stripe.controller.ts
@@ -1,26 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_TEST as string);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET_TEST as string;
 
+let prisma = new PrismaClient();
+
 export const webhook = async (req: Request, res: Response) => {
+  console.log("Webhook received");
   const sig = req.headers["stripe-signature"] as string | undefined;
 
   if (!sig) {
     throw new Error("Stripe signature not provided");
   }
 
+  let data;
   let event: Stripe.Event;
+  let eventType: Stripe.Event.Type;
 
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
   } catch (error) {
+    console.log(`Webhook Error: ${error}`);
     throw new Error(`Webhook Error: ${error}`);
   }
 
+  data = event.data;
+  eventType = event.type;
+
   // Handle the event
-  switch (event.type) {
+  switch (eventType) {
     case "payment_intent.succeeded":
       const paymentIntent = event.data.object as Stripe.PaymentIntent;
       console.log("PaymentIntent was successful!");
@@ -29,7 +39,31 @@ export const webhook = async (req: Request, res: Response) => {
       const paymentMethod = event.data.object as Stripe.PaymentMethod;
       console.log("PaymentMethod was attached to a Customer!");
       break;
-    // ... handle other event types
+    case "checkout.session.completed":
+      // Payment is successful and the subscription is created.
+      const session = event.data.object as Stripe.Checkout.Session;
+      // Récupérer l'email du client depuis la session
+      const customerEmail = session.customer_details?.email;
+
+      if (customerEmail) {
+        // Rechercher un utilisateur avec cet email
+        let user = await prisma.user.findUnique({
+          where: { email: customerEmail },
+        });
+
+        // Si l'utilisateur n'existe pas, en créer un nouveau
+        if (!user) {
+          console.log(`New user created with email: ${customerEmail}`);
+        } else {
+          // Si l'utilisateur existe, mettez à jour son accès
+
+          console.log(
+            `Access granted to existing user with email: ${customerEmail}`,
+          );
+        }
+      } else {
+        console.error("No customer email found in session");
+      }
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
