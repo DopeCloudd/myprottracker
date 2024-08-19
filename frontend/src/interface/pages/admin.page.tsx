ee1ee4e98df1caddd3b1b254da2c2ec335687087
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-19 13:35:12 +0200

    refactor: Update admin page to use StatsCards component for displaying site-wide statistics

--- frontend/src/interface/pages/admin.page.tsx
+++ frontend/src/interface/pages/admin.page.tsx
@@ -1,4 +1,4 @@
-import useStats from "@/application/hooks/useStats";
+import StatsCards from "@/interface/components/admin/stats.cards";
 import FlexCenter from "@/interface/components/box/flex-center.component";
 import ProductList from "@/interface/components/table/product-list.table";
 import RequestsTable from "@/interface/components/table/requests.table";
@@ -6,15 +6,13 @@ import LanguageIcon from "@mui/icons-material/Language";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Box, Grid, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Admin: React.FC = () => {
   const navigate = useNavigate();
-  const { stats } = useStats();
-  console.log(stats);
 
   return (
     <Box
@@ -57,59 +55,7 @@ const Admin: React.FC = () => {
           Configuration
         </Button>
       </FlexCenter>
-      <Grid
-        container
-        spacing={3}
-        sx={{
-          py: 4,
-        }}
-      >
-        <Grid item xs={6} md={4}>
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            {stats?.count_brands} marques
-          </Paper>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            {stats?.count_categories} catégories
-          </Paper>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            {stats?.count_products} produits
-          </Paper>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            {stats?.count_requests} demandes
-          </Paper>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            {stats?.count_users} utilisateurs
-          </Paper>
-        </Grid>
-      </Grid>
+      <StatsCards />
       <RequestsTable />
       <ProductList maxHeight={400} />
     </Box>
