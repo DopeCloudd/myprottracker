7dea70bf21db993bd85ff78b657afa3b4ce5dfed
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-20 14:01:30 +0200

    refactor: Update dotenv configuration to load environment variables based on NODE_ENV

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import app from "./app";
 
-dotenv.config();
+dotenv.config(); // Charge le .env par défaut
+const env = process.env.NODE_ENV || "dev";
+dotenv.config({ path: `.env.${env}` });
 
 const PORT = process.env.API_PORT || 3032;
 app.listen(PORT, () => {
