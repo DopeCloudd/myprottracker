8c8fa73c26995a91c500f28c7edacd0f6b06f39f
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-20 14:01:26 +0200

    refactor: Add createCheckoutSession function for handling Stripe checkout sessions

--- backend/src/controllers/stripe.controller.ts
+++ backend/src/controllers/stripe.controller.ts
@@ -7,6 +7,40 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET_TEST as string;
 
 let prisma = new PrismaClient();
 
+// Create a checkout session
+export const createCheckoutSession = async (req: Request, res: Response) => {
+  const { userId } = req.body;
+
+  // Get the user's email
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card"],
+    customer_email: user.email,
+    line_items: [
+      {
+        price: "price_1PbhjjLgRo1w7m00AcJ5MOfI",
+        quantity: 1,
+      },
+    ],
+    metadata: {
+      userId: user.id,
+    },
+    success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.CLIENT_URL}/cancel`,
+  });
+
+  res.json({ id: session.id, url: session.url });
+};
+
+// Webhook handler
 export const webhook = async (req: Request, res: Response) => {
   console.log("Webhook received");
   const sig = req.headers["stripe-signature"] as string | undefined;
@@ -15,7 +49,6 @@ export const webhook = async (req: Request, res: Response) => {
     throw new Error("Stripe signature not provided");
   }
 
-  let data;
   let event: Stripe.Event;
   let eventType: Stripe.Event.Type;
 
@@ -26,44 +59,18 @@ export const webhook = async (req: Request, res: Response) => {
     throw new Error(`Webhook Error: ${error}`);
   }
 
-  data = event.data;
   eventType = event.type;
+
+  const session = event.data.object as Stripe.Checkout.Session;
 
   // Handle the event
   switch (eventType) {
-    case "payment_intent.succeeded":
-      const paymentIntent = event.data.object as Stripe.PaymentIntent;
-      console.log("PaymentIntent was successful!");
-      break;
-    case "payment_method.attached":
-      const paymentMethod = event.data.object as Stripe.PaymentMethod;
-      console.log("PaymentMethod was attached to a Customer!");
-      break;
     case "checkout.session.completed":
-      // Payment is successful and the subscription is created.
-      const session = event.data.object as Stripe.Checkout.Session;
-      // Récupérer l'email du client depuis la session
-      const customerEmail = session.customer_details?.email;
-
-      if (customerEmail) {
-        // Rechercher un utilisateur avec cet email
-        let user = await prisma.user.findUnique({
-          where: { email: customerEmail },
-        });
-
-        // Si l'utilisateur n'existe pas, en créer un nouveau
-        if (!user) {
-          console.log(`New user created with email: ${customerEmail}`);
-        } else {
-          // Si l'utilisateur existe, mettez à jour son accès
-
-          console.log(
-            `Access granted to existing user with email: ${customerEmail}`,
-          );
-        }
-      } else {
-        console.error("No customer email found in session");
-      }
+      console.log("Checkout session completed");
+      const subscription = stripe.subscriptions.retrieve(
+        session.subscription as string,
+      );
+      console.log("Subscription : ", subscription);
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
