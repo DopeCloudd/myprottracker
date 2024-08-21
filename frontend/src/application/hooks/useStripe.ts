8d62f0f2f61fd5332c3e71861dd5557c680b55f2
Valentin Lerouge <cloudd2vi@gmail.com>
2024-08-21 13:53:23 +0200

    refactor: Update useStripe hook to include error handling and snackbar notification

--- frontend/src/application/hooks/useStripe.ts
+++ frontend/src/application/hooks/useStripe.ts
@@ -1,12 +1,14 @@
 import { useAuth } from "@/application/hooks/useAuth";
 import { useCreateCheckoutSessionMutation } from "@/infrastructure/api/stripe.api";
+import { useSnackbar } from "notistack";
 
 // Hook for using Stripe API
 const useStripe = () => {
   const { user } = useAuth();
   const [createCheckoutSession] = useCreateCheckoutSessionMutation();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const getCheckoutUrl = async () => {
+  const createCheckoutUrl = async () => {
     if (!user) {
       console.error("User not found");
     } else {
@@ -17,12 +19,15 @@ const useStripe = () => {
         return result.url; // renvoie l'URL si la requête a réussi
       } catch (error) {
         console.error("Failed to create checkout session:", error);
+        enqueueSnackbar("Failed to create checkout session", {
+          variant: "error",
+        });
         throw error; // lance l'erreur si la requête a échoué
       }
     }
   };
 
-  return { getCheckoutUrl };
+  return { createCheckoutUrl };
 };
 
 export default useStripe;
