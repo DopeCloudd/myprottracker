a556270261324a479d8bf0515581b89b19c62450
Valentin Lerouge <cloudd2vi@gmail.com>
2024-09-08 00:29:25 +0200

    refactor: Update sendEmail.ts to improve email functionality

--- backend/src/mail/sendEmail.ts
+++ backend/src/mail/sendEmail.ts
@@ -1,13 +1,13 @@
 import transporter from "./mailer";
-import { renderWelcomeEmail } from "./renderEmail";
+import { renderLowestPriceEmail, renderWelcomeEmail } from "./renderEmail";
 
-export const sendWelcomeEmail = async (to: string, email: string) => {
+export const sendWelcomeEmail = async (email: string) => {
   const htmlContent = renderWelcomeEmail({ email });
 
   const mailOptions = {
     from: "noreply@myprottracker.com", // L'adresse email de l'expéditeur
-    to, // L'adresse email du destinataire
-    subject: "Welcome to MyProtTrqcker", // Sujet de l'email
+    to: email, // L'adresse email du destinataire
+    subject: "Welcome to MyProtTracker", // Sujet de l'email
     html: htmlContent, // Contenu HTML de l'email
   };
 
@@ -15,5 +15,33 @@ export const sendWelcomeEmail = async (to: string, email: string) => {
     await transporter.sendMail(mailOptions);
   } catch (error) {
     throw new Error(`Error sending email: ${error}`);
+  }
+};
+
+export const sendLowestPriceEmail = async (
+  email: string,
+  productId: string,
+) => {
+  const htmlContent = renderLowestPriceEmail({ productId });
+
+  const mailOptions = {
+    from: "noreply@myprottracker.com", // L'adresse email de l'expéditeur
+    to: email, // L'adresse email du destinataire
+    subject: "Alerte - Baisse de prix !", // Sujet de l'email
+    html: htmlContent, // Contenu HTML de l'email
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return { success: true, messageId: info.messageId };
+  } catch (error) {
+    // Safely handle unknown error types
+    let errorMessage = "An unknown error occurred";
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === "string") {
+      errorMessage = error; // If error is a string, handle it directly
+    }
+    return { success: false, error: errorMessage }; // Return failure status without throwing
   }
 };
