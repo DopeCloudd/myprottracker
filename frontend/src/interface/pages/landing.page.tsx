9d4a077d9b52c247aa97187646b76b66165b26e7
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-09 13:35:14 +0200

    chore: use good file to display CTA

--- frontend/src/interface/pages/landing.page.tsx
+++ frontend/src/interface/pages/landing.page.tsx
@@ -1,15 +1,15 @@
 import FlexColumnCenter from "@/interface/components/box/flex-column-center.component";
-import CallToAction from "@/interface/components/landing/CallToAction";
 import Hero from "@/interface/components/landing/Hero";
 import Highlights from "@/interface/components/landing/Highlights";
 import Title from "@/interface/components/landing/Title";
 import PricingSection from "@/interface/components/sections/pricing.section";
+import CtaButton from "@/interface/components/button/cta.button";
 
 export default function Landing() {
   return (
     <FlexColumnCenter>
       <Title />
-      <CallToAction />
+      <CtaButton />
       <Hero />
       <Highlights />
       <PricingSection />
