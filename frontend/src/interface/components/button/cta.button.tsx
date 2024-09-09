45bdb330648e8d9fbdd76216bfed5b61b8072d8e
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-09 13:34:47 +0200

    chore: create CTA button in button components

---
+++ frontend/src/interface/components/button/cta.button.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, alpha } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+const ActionButton = styled(Button)(() => ({
+  width: "100%",
+  padding: "0.6em 2em",
+  borderRadius: "18px",
+  outline: "2px solid",
+  outlineColor: alpha("#00A656", 0.1),
+  boxShadow: `0 0 24px 12px ${alpha("#00A656", 0.2)}`,
+}));
+
+export default function CtaButton() {
+  // For translation
+  const { t } = useTranslation();
+  // For navigation
+  const navigate = useNavigate();
+  // Go to filtres page
+  const handleButtonClick = () => {
+    navigate("/categories");
+  };
+
+  return (
+    <Box
+      id="calltoaction"
+      sx={{
+        pt: 4,
+        pb: 4,
+        width: "100%",
+        backgroundColor: "#171717",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          height: "fit-content",
+          width: { xs: "70%", md: "50%" },
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <ActionButton onClick={handleButtonClick}>
+          {t("home.button")}
+        </ActionButton>
+      </Box>
+    </Box>
+  );
+}
