c5fe66a2ac2ca871bdc2cc967fa8ad31a13b2fc1
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-11 13:52:18 +0200

    chore: implement webhook Stripe

--- backend/src/controllers/stripe.controller.ts
+++ backend/src/controllers/stripe.controller.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Plan, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import Stripe from "stripe";
 
@@ -62,35 +62,79 @@ export const webhook = async (req: Request, res: Response) => {
     console.log(`Webhook Error: ${error}`);
     throw new Error(`Webhook Error: ${error}`);
   }
-
+  // Get the event type
   eventType = event.type;
-
-  const session = event.data.object as Stripe.Checkout.Session;
+  // Get the data
+  const data = event.data.object as Stripe.Checkout.Session;
 
   // Handle the event
-  switch (eventType) {
-    // Event when the subscription is started
-    case "checkout.session.completed":
-      console.log("Checkout session completed");
-      const subscription = stripe.subscriptions.retrieve(
-        session.subscription as string,
-      );
-      console.log("Subscription : ", subscription);
-      break;
-    // Event when the payment is successful (every subscription interval)
-    case "invoice.paid":
-      console.log("Invoice paid");
-      break;
-    // Event when the payment failed due to card issues (every subscription interval)
-    case "invoice.payment_failed":
-      console.log("Invoice payment failed");
-      break;
-    // Event when the subscription is updated
-    case "customer.subscription.updated":
-      console.log("Customer subscription updated");
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+  try {
+    switch (eventType) {
+      // Event when the subscription is started
+      case "checkout.session.completed":
+        console.log("Checkout session completed");
+        // Get the subscription details
+        const session = await stripe.checkout.sessions.retrieve(data.id, {
+          expand: ["subscription", "line_items"],
+        });
+        // Get the customer details
+        const customerId = session.customer as string;
+        const customerDettails = session.customer_details;
+        // If the customer has an email, update the user's stripeCustomerId
+        if (customerDettails?.email) {
+          // Find the user in the database
+          const user = await prisma.user.findUnique({
+            where: { email: customerDettails.email },
+          });
+          // If the user is not found, throw an error
+          if (!user) throw new Error("User not found");
+          // If the user does not have a stripeCustomerId, update it
+          if (!user.stripeCustomerId) {
+            await prisma.user.update({
+              where: { id: user.id },
+              data: { stripeCustomerId: customerId },
+            });
+          }
+          let endDate = new Date();
+          endDate.setMonth(endDate.getMonth() + 1);
+          await prisma.subscription.upsert({
+            where: { userId: user.id },
+            update: {
+              endDate: endDate,
+            },
+            create: {
+              userId: user.id,
+              startDate: new Date(),
+              endDate: endDate,
+            },
+          });
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { plan: Plan.PREMIUM },
+          });
+        }
+        break;
+      case "customer.subscription.deleted":
+        const subscription = await stripe.subscriptions.retrieve(data.id);
+        const user = await prisma.user.findUnique({
+          where: { stripeCustomerId: subscription.customer as string },
+        });
+        if (user) {
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { plan: Plan.FREE },
+          });
+        } else {
+          console.error("User not found for the subscription deleted event.");
+          throw new Error("User not found for the subscription deleted event.");
+        }
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  } catch (error) {
+    console.log(`Error handling the event: ${error}`);
+    throw new Error(`Error handling the event: ${error}`);
   }
 
   res.json({ received: true });
