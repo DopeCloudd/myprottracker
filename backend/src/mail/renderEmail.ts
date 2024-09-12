339b926737f7f97d991404518f4f93113f0f3720
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-12 13:48:13 +0200

    chore: create function to render reset password email

--- backend/src/mail/renderEmail.ts
+++ backend/src/mail/renderEmail.ts
@@ -2,7 +2,7 @@ import { render } from "@react-email/render";
 import React from "react";
 import LowestPrice from "./emailTemplates/LowestPrice";
 import WelcomeEmail from "./emailTemplates/WelcomeEmail";
-
+import ResetPasswordEmail from "./emailTemplates/ResetPasswordEmail";
 type WelcomeEmailProps = {
   email: string;
 };
@@ -17,4 +17,12 @@ type LowestPriceProps = {
 
 export const renderLowestPriceEmail = (props: LowestPriceProps) => {
   return render(React.createElement(LowestPrice, props));
+};
+
+type ResetPasswordProps = {
+  token: string;
+};
+
+export const renderResetPasswordEmail = (props: ResetPasswordProps) => {
+  return render(React.createElement(ResetPasswordEmail, props));
 };
