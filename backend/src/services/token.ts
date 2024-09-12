40557b27a42e41e515c7fa3e5314031dff56834c
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-12 13:47:00 +0200

    chore: create token to handle reset password, token is valid one hour

--- backend/src/services/token.ts
+++ backend/src/services/token.ts
@@ -10,3 +10,18 @@ export const generateToken = (userId: string): string => {
     expiresIn: "1d",
   });
 };
+
+// Create a token for reset password (without jwt)
+export const generatePasswordResetToken = (email: string): string => {
+  // Create a token payload with the user's email
+  const payload = { email };
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  // Generate a JWT token with the payload, secret key, and expiration time
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  return token;
+};
